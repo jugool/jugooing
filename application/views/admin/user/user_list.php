84766e8543786f4fc5f7360381ed6d45fb19abae
Steven.lin <linjiaqi411@gmail.com>
2015-07-24 09:54:08 +0800

    用户管理-列表
    新增用户列表页样式

--- application/views/admin/user/user_list.php
+++ application/views/admin/user/user_list.php
@@ -1,11 +1,119 @@
 <script type="text/javascript" src="<?php echo base_url(); ?>public/js/jquery.js"></script>
-<link rel="stylesheet" href="<?php echo base_url(); ?>public/css/add.css" type="text/css" media="screen">
-<link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/bootstrap.css" type="text/css" media="screen">
-<link rel="stylesheet" href="<?php echo base_url(); ?>public/css/index.css" type="text/css" media="screen">
-<div class="route_bg">
-    <a href="#">主页</a><i class="glyph-icon icon-chevron-right"></i>
-    <a href="#">用户列表</a>
+<link rel="stylesheet" href="<?php echo base_url(); ?>public/css/bootstrap.css" type="text/css" media="screen">
+<link rel="stylesheet" href="<?php echo base_url(); ?>public/css/admin.css" type="text/css" media="screen">
+<ol class="breadcrumb">
+    <li><a href="#">用户管理</a></li>
+    <li class="active">用户列表</li>
+</ol>
+
+<div class="table_list">
+    <form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">查询</button>
+    </form>
 </div>
-<div class="div_from_aoto" style="width: 500px;">
-	用户列表页面
-</div>
+
+<table class="table table-striped table-bordered table-hover table-condensed">
+    <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>工号</th>
+        <th>手机</th>
+        <th>邮箱</th>
+        <th>用户类型</th>
+        <th>创建时间</th>
+        <th>操作</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>佳佳</td>
+        <td>257</td>
+        <td>1523655484</td>
+        <td>jijia@@qq.com</td>
+        <td>普通用户</td>
+        <td>2014/10/23</td>
+        <td>
+            <a href="javascript:;">修改</a> |
+            <a href="javascript:;">删除</a>
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>佳佳</td>
+        <td>257</td>
+        <td>1523655484</td>
+        <td>jijia@@qq.com</td>
+        <td>普通用户</td>
+        <td>2014/10/23</td>
+        <td>
+            <a href="javascript:;">修改</a> |
+            <a href="javascript:;">删除</a>
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>佳佳</td>
+        <td>257</td>
+        <td>1523655484</td>
+        <td>jijia@@qq.com</td>
+        <td>普通用户</td>
+        <td>2014/10/23</td>
+        <td>
+            <a href="javascript:;">修改</a> |
+            <a href="javascript:;">删除</a>
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>佳佳</td>
+        <td>257</td>
+        <td>1523655484</td>
+        <td>jijia@@qq.com</td>
+        <td>普通用户</td>
+        <td>2014/10/23</td>
+        <td>
+            <a href="javascript:;">修改</a> |
+            <a href="javascript:;">删除</a>
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>佳佳</td>
+        <td>257</td>
+        <td>1523655484</td>
+        <td>jijia@@qq.com</td>
+        <td>普通用户</td>
+        <td>2014/10/23</td>
+        <td>
+            <a href="javascript:;">修改</a> |
+            <a href="javascript:;">删除</a>
+        </td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>佳佳</td>
+        <td>257</td>
+        <td>1523655484</td>
+        <td>jijia@@qq.com</td>
+        <td>普通用户</td>
+        <td>2014/10/23</td>
+        <td>
+            <a href="javascript:;">修改</a> |
+            <a href="javascript:;">删除</a>
+        </td>
+    </tr>
+</table>
+<nav>
+    <ul class="pagination">
+        <li><a href="javascript:;">总页数：27</a></li>
+        <li class="disabled"><a aria-label="Previous" href="#"><span aria-hidden="true">«</span></a></li>
+        <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li><a aria-label="Next" href="#"><span aria-hidden="true">»</span></a></li>
+    </ul>
+</nav>
